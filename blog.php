1dbc84938c45e31b61de66b617b9e892c138e0fa
kiou <quentin.far@gmail.com>
2022-05-11 14:22:30 +0200

    MAJ

---
+++ blog.php
@@ -0,0 +1,27 @@
+<?php
+
+Use Blog\Article;
+Use Blog\Category;
+
+require __DIR__.'/vendor/autoload.php';
+
+$categories = [
+new Category(1, 'foo', null),
+new Category(2, 'bar', null),
+new Category(3, 'baz', null)];
+dump($categories);
+
+$articles = [
+    new Article(1,'lorem','ipsum',$categories[0]),
+    new Article(2,'ipsum','lorem', $categories[1]),
+    new Article(3, 'pekat','lorem ipsum', $categories[1])
+];
+dump($articles);
+
+foreach($articles as $article){
+    echo$article ->getTitle();
+    echo'<br>';
+    echo $article->getCategory()->getName();
+    echo '<br>';
+}
+
